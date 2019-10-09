c869363eaf40350f0f89685668745f4da344bbfb
Randall <rfields1128@yahoo.com>
2019-10-09 14:22:20 -0500

    adding basic server functionality and some get requests for future use

---
+++ server.js
@@ -0,0 +1,55 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser")
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.listen(PORT, function () {
+    console.log("listening on PORT" + PORT)
+});
+
+var data = [
+    {
+        reservation: [],
+        waitingList: []
+    }
+];
+
+var visitorCount = 0;
+app.get("/table", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/api/home", function (req, res) {
+    res.json(data.reservations)
+});
+
+app.get("/api/waitlist", function (req, res) {
+    res.json(data.waitingList)
+});
+
+app.get("/api", function (req, res) {
+    res.json(data);
+});
+
+app.get("/api/clear", function (req, res) {
+    data.reservations.length = 0;
+    data.waitingList.length = 0;
+    res.json(data);
+});
+
+
+app.get("/api/visitors", function (req, res) {
+    res.json(visitorCount);
+});
+
+
+app.post("/api/new", function(req, res){
+    var tableData = req.body;
+    console.log(tableData)
+});
+
+
